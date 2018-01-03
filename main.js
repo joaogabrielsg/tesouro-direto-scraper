f691242f36ac54794cdeec70a21dcfe6dba59e99
joaogabrieldosantosgoncalves@gmail.com <joaogabrieldosantosgoncalves@gmail.com>
2018-01-03 17:29:17 -0300

    Request html from url

--- main.js
+++ main.js
@@ -2,13 +2,20 @@ const express = require('express');
 const app = express();
 
 const request = require('request');
+const cheerio = require('cheerio');
 
 const url = 'http://www.tesouro.fazenda.gov.br/tesouro-direto-precos-e-taxas-dos-titulos';
 
 app.get('/', (req, res) => {
     request(url, (error, response, html) => {
-        res.send(html);
+        if(!error){
+            var $ = cheerio.load(html);
+
+            $('td.listing').each(function(index, element) {
+                console.log($(this).text());
+            })
+        }
     })
 });
 
-app.listen(8080, () => console.log('Example app listening on port 3000!'));
+app.listen(8080, () => console.log('Example app listening on port 8080!'));
---
+++ request-html.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const request = require('request');
+
+const requestHTML = (url, callback) => {
+    app.get('/', (req, res) => {
+        request(url, (error, response, html) => {
+            if(!error){
+                callback(null, html);
+            }else{
+                callback(error);
+            }
+        })
+    });
+};
+
+app.listen(8080, () => console.log('Example app listening on port 8080!'));
+
+module.exports = requestHTML;
